93cf3a0ed4e0f8c28fef190656a8461608cb0e3d
Ayberk Arıcı <53842315+karicii@users.noreply.github.com>
2025-08-29 16:15:24 +0300

    .

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,33 +1,33 @@
 <template>
-<div id="app">
-<nav>
-<router-link to="/">Anasayfa</router-link> |
-<router-link v-if="!isLoggedIn" to="/login">Giriş Yap</router-link>
-<router-link v-if="isLoggedIn" to="/admin">Yönetim Paneli</router-link>
-<a href="#" v-if="isLoggedIn" @click.prevent="logout">Çıkış Yap</a>
-</nav>
-<router-view />
-</div>
+  <div id="app-wrapper">
+    <header class="main-header">
+      <div class="header-container">
+        <router-link to="/" class="logo">ayberk.one</router-link>
+        <nav class="main-nav">
+          <router-link to="/">Yazılar</router-link>
+          <router-link v-if="isLoggedIn" to="/admin">Yönetim Paneli</router-link>
+          <a href="#" v-if="isLoggedIn" @click.prevent="logout" class="logout-button">Çıkış Yap</a>
+          <router-link v-if="!isLoggedIn" to="/login" class="login-button">Giriş Yap</router-link>
+        </nav>
+      </div>
+    </header>
+    <main class="main-content">
+      <router-view />
+    </main>
+  </div>
 </template>
 
-<script setup> 
+<script setup>
 import { ref, watchEffect } from 'vue';
 import { useRouter } from 'vue-router';
-
 const router = useRouter();
 const isLoggedIn = ref(!!localStorage.getItem('token'));
-
 watchEffect(() => {
-isLoggedIn.value = !!localStorage.getItem('token');
+  isLoggedIn.value = !!localStorage.getItem('token');
 });
-
 const logout = () => {
-localStorage.removeItem('token');
-isLoggedIn.value = false;
-router.push('/login');
+  localStorage.removeItem('token');
+  isLoggedIn.value = false;
+  router.push('/login');
 };
 </script>
-
-<style> 
-
-</style>
